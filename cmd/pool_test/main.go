c6ed8add1b6cd73fd0f6baa0ed6c47d1b37bf770
Hurricanezwf <1094646850@qq.com>
2017-12-01 01:58:10 +0800

    升级并发控制工具支持动态设定并发上限

--- cmd/pool_test/main.go
+++ cmd/pool_test/main.go
@@ -2,26 +2,69 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime/pprof"
+	"syscall"
 	"time"
 
 	"github.com/Hurricanezwf/toolbox/pool"
 )
 
-func main() {
-	c := pool.NewConcurrency(2)
+var (
+	concurrency = 1
+	c           = pool.NewConcurrency(concurrency)
+)
 
-	for i := 0; i < 100; i++ {
-		c.Get()
-		go Do(i, c)
+func main() {
+	go DoWork()
+
+	go func() {
+		http.HandleFunc("/", handler)
+		http.ListenAndServe("127.0.0.1:9000", nil)
+	}()
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGUSR1, syscall.SIGUSR2, syscall.SIGTERM)
+	for {
+		s := <-sig
+		if s == syscall.SIGTERM {
+			break
+		}
+
+		if s == syscall.SIGUSR1 {
+			fmt.Printf("Receive add concurrency\n")
+			concurrency += 2
+			c.ResetMax(concurrency)
+		} else if s == syscall.SIGUSR2 {
+			fmt.Printf("Receive reduce concurrency\n")
+			concurrency -= 3
+			c.ResetMax(concurrency)
+		} else {
+			fmt.Printf("Unkonw signal %v\n", s)
+		}
 	}
 }
 
-func Do(idx int, c *pool.Concurrency) {
-	fmt.Printf("%d is running...\n", idx)
+func DoWork() {
+	for {
+		c.Get()
+		go Do(c)
+	}
+}
+
+func Do(c *pool.Concurrency) {
 	defer func() {
-		fmt.Printf("%d stopped\n", idx)
 		c.Put()
 	}()
 
-	time.Sleep(3 * time.Second)
+	time.Sleep(5 * time.Second)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain")
+
+	p := pprof.Lookup("goroutine")
+	p.WriteTo(w, 1)
 }
--- pool/pool.go
+++ pool/pool.go
@@ -1,15 +1,23 @@
 package pool
 
+import (
+	"sync"
+	"time"
+)
+
+// 保证并发数至少为1
 type Concurrency struct {
-	// max表示允许的最大并发量
-	max int
+	mutex  sync.Mutex
+	curNum int // 当前并发数
+
+	max int // max表示允许的最大并发量
 
 	ch chan struct{}
 }
 
 func NewConcurrency(max int) *Concurrency {
 	if max <= 0 {
-		return nil
+		max = 1
 	}
 
 	ch := make(chan struct{}, max)
@@ -23,10 +31,71 @@ func NewConcurrency(max int) *Concurrency {
 	}
 }
 
+// ResetMax: 重新设置最大并发量
+func (c *Concurrency) ResetMax(max int) {
+	if max <= 0 {
+		max = 1
+	}
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if c.max == max {
+		return
+	}
+
+	var (
+		reput = 0
+		newCh = make(chan struct{}, max)
+	)
+
+	if max > c.max {
+		reput = max - c.curNum
+		if reput < 0 {
+			reput = 0
+		}
+		for reput > 0 {
+			reput--
+			newCh <- struct{}{}
+		}
+	} else if max < c.max {
+		if c.curNum < max {
+			reput = max - c.curNum
+		}
+		for reput > 0 {
+			reput--
+			newCh <- struct{}{}
+		}
+	}
+
+	close(c.ch)
+	c.ch = newCh
+	c.max = max
+}
+
 func (c *Concurrency) Get() {
-	<-c.ch
+	for {
+		if _, ok := <-c.ch; ok {
+			break
+		}
+		time.Sleep(1 * time.Millisecond)
+	}
+
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.curNum++
 }
 
 func (c *Concurrency) Put() {
-	c.ch <- struct{}{}
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.curNum--
+	if c.curNum < 0 {
+		c.curNum = len(c.ch)
+	}
+	if c.curNum < c.max {
+		c.ch <- struct{}{}
+	}
 }
